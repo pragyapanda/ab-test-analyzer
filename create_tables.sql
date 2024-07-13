c2550069083c58bcb0cf99de4af9c24d4cf7b4c7
pragyapanda <100715114+pragyapanda@users.noreply.github.com>
2024-07-13 13:43:45 +0200

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users_a (
+    user_id INT PRIMARY KEY,
+    signup_date DATE,
+    signup_method VARCHAR(50)
+);
+
+CREATE TABLE users_b (
+    user_id INT PRIMARY KEY,
+    signup_date DATE,
+    signup_method VARCHAR(50)
+);
